b08ac3cc56e03293f43cb0222fc144c2a4652005
Nikhil4853 <nikhilkashyap.may021@gmail.com>
2022-04-01 06:06:59 +0530

    element appear O(n)

--- 25_Element_appear_nBYk_time.cpp
+++ 25_Element_appear_nBYk_time.cpp
@@ -3,7 +3,8 @@ using namespace std;
 class elementAppear
 {
 public:
-// time O(nlogn)
+    // time O(nlogn)
+    // space O(1)
     void findElement(int arr[], int size, int k)
     {
         int x = size / k;
@@ -26,11 +27,28 @@ public:
             }
         }
     }
+    // time O(n) and space O(n)
+    void element(int arr[], int size, int k)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < size; i++)
+        {
+            m[arr[i]] += 1;
+        }
+        for (auto it : m)
+        {
+            // it.second =is value of the map 
+            if(it.second>k)
+            { // it.first = is key of the map 
+                cout << it.first<<endl;}
+        }
+    }
 };
 int main()
 {
     int arr[] = {2, 21, 5, 3, 4, 4, 5, 5, 5, 5, 5, 55, 5, 6, 6, 7, 8, 86, 3, 3, 5, 1, 5, 3};
     elementAppear eA;
-    eA.findElement(arr, 24, 3);
+  //  eA.findElement(arr, 24, 3);
+    eA.element(arr,24,2);
     return 0;
 }
